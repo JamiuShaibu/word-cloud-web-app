7d1d9ceaa80b0a2571abb3bb9f8c7f8358656adf
Jamiu Shaibu <jamiushaibu12@gmail.com>
2023-05-11 11:41:56 +0200

    Removed word(s) from stopwords

--- scripts/project_modules/enforce_stopwords.py
+++ scripts/project_modules/enforce_stopwords.py
@@ -11,13 +11,13 @@ def provide_more_stopwords(language: str) -> list[str]:
         "english": [
             'doi', 'also', 'often', 'much', 'even', 'see', 'could', 'seem', 'others', 'us', 'https',
             'www', 'said', 'end', 'next', 'likely', 'whether', 'another', 'going', 'go', 'without',
-            'would', 'doing', 'someone', 'one', 'thing', 'two', 'no', 'Whenever', 'every', 'second',
-            'seconds', 'something', 'somethings', 'things', 'used', 'com', 'However', 'rather', 'still',
-            'put', 'que', 'p', 'la', 'de', 'pp', 'las', 'Thus', 'ed', 'en', 'vol', 'th', 'a', 'b', 'c',
-            'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
-            'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O',
-            'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'seems', 'come', 'Come', 'whenever',
-            'three', 'yet', 'way', 'TXT', 'txt', 'pc', 'either', 'ing', 'else', 'ever', 'therefore',
+            'would', 'doing', 'someone', 'one', 'thing', 'two', 'no', 'Whenever', 'every', 'something',
+            'somethings', 'things', 'used', 'com', 'However', 'rather', 'still', 'put', 'que', 'p', 'la',
+            'de', 'pp', 'Thus', 'ed', 'en', 'vol', 'th', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+            'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C',
+            'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
+            'W', 'X', 'Y', 'Z', 'seems', 'come', 'Come', 'whenever', 'three', 'yet', 'way', 'TXT', 'txt',
+            'pc', 'either', 'ing', 'else', 'ever', 'therefore'
         ],
         "spanish": [
             'solo', 'somos', 'tan', 'aun', 'mismo', 'misma', 'mas', 'si', 'sere', 'veras', 'aqui', 'ser',
